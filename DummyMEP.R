7cdcf2216793e0bb2567d17842b7d07dc1ec7889
Mepoide <marcusbrodie@gmail.com>
2016-11-12 11:16:10 +0100

    first commit

---
+++ DummyMEP.R
@@ -0,0 +1,13 @@
+
+
+## Dummy
+
+for (i in 1:10) print('Hola Mundo') 
+
+timestamp()
+
+library(dummies)
+
+letters <- c( "a", "a", "b", "c", "d", "e", "f", "g", "h", "b", "b" )
+dummy( as.character(letters) )
+dummy( letters[1:6] )
